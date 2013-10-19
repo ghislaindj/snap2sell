f276e2458fdaf1a3032a483714a0505f2c6eea17
Ghislain de Juvigny <gdjuvigny@gmail.com>
2013-10-19 14:21:39 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Snap2sell::Application.config.secret_key_base = '1df455c26921d1ed3fe6713564e34a291e56262b0eabfe835ff8a19a7f57c0f50bbc61d8a9c09ec229705cf05958511e041816b577e1b0c3cc51f43e81c7248e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Snap2sell::Application.config.session_store :cookie_store, key: '_snap2sell_session'
