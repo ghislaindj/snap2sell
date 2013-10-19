5415db131e71e2e4ff0f5b662097b6da9b8bdb8f
Ghislain de Juvigny <gdjuvigny@gmail.com>
2013-10-19 15:06:35 +0200

    fix pciture

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,11 @@
 class Product
   include Mongoid::Document
   include Mongoid::Timestamps
+  include Mongoid::Paperclip
+
 
   belongs_to :user,       class_name: 'User'
-  has_mongoid_attached_file :picture
+  has_mongoid_attached_file :picture, styles: {thumb: "340x340#"}
 
   field :title, type: String
 end
